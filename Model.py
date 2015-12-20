944c3d4141a8e335074e52b28a17852c68c4be7c
brilliancy <alanju@Alans-MacBook-Pro.local>
2015-12-20 18:29:13 -0500

    Updates to model2

--- Model.py
+++ Model.py
@@ -96,8 +96,8 @@ def main():
 		#labelDf = labelDf[:550]
 		labelDf.Finish.unique() # find unique values
 
-		labelDf.Finish = labelDf.Finish.replace(' N/A','20')
-		labelDf.Finish = labelDf.Finish.replace(' 2 2','2')
+		labelDf.Finish = labelDf.Finish.replace(' N/A','20',regex=True)#reg
+		labelDf.Finish = labelDf.Finish.replace(' 2 2','2', regex=True)
 		labelDf.Finish = labelDf.Finish.replace('st','',regex=True)
 
 		labelDf.Finish = labelDf.Finish.apply(lambda k: int(k))
@@ -172,30 +172,27 @@ def main():
 	subset_1 = masterDf[(masterDf.begin > sub_datetime("00:28:34")) & (masterDf.begin < sub_datetime("00:36:14")) ]
 
 	#relevant target variables for Model 2
-	labelDf = labelDf[["Contestant_names","Finish"]]
+	
+	labelDf = labelDf[["Contestant_names","Finish","Season"]]
 
+	def Label_subset_by_season(Season_begin,Season_end):
+		#subset label by season
+		label_sub_season = labelDf[(labelDf.Season >= Season_begin) & (labelDf.Season <= Season_end)]
+		return label_sub_season
+	label_sub_season = Label_subset_by_season(28,30)
 
-	#subset some seasons just to see if it works at least
-	def subset_by_season(Season_begin,Season_end):
+	#subset some seasons in MasterDf just to see if it works at least
+	def Master_subset_by_season(Season_begin,Season_end):
 		#temporarily subset_2 instead of masterDf
 		subset_2 = subset_1[(subset_1.Season >= Season_begin) & (subset_1.Season <= Season_end)]
 		return subset_2
 
-	subset_3 = subset_by_season(28,30)
+	master_sub_season = Master_subset_by_season(28,30)
 
-	#get unique episodes from subset_3
+	#get token episodes from subset by season
 
 	def get_ep_tokens(EpID):
-		subset_episode = subset_3[subset_3.EpisodeId == EpID].copy()
-		#subset_episode['subtitles_text'] = subset_episode.subtitles_text.apply(lambda k: k + ' ')
-
-
-		# df.loc[:, cols] = df.loc[:, cols].applymap(some_function) 
-		#cols = 'subtitles_text'
-		#subset_episode.loc[:, cols] = subset_episode.loc[:, cols].apply(lambda k: k + ' ') 
-
-		#labelDf.Tribal_Challg_Wins = labelDf.Tribal_Challg_Wins.apply(lambda k: int(k))
-		#subset_episode.subtitles_text = subset_episode.subtitles_text.apply(lambda k: k + ' ')
+		subset_episode = master_sub_season[master_sub_season.EpisodeId == EpID].copy()
 	
 		text = ' '.join(subset_episode.subtitles_text) 
 		lowers = text.lower()
@@ -205,55 +202,82 @@ def main():
 		#tokens = nltk.word_tokenize(no_punctuation)
 		tokenizer = RegexpTokenizer('\s+', gaps=True)
 		#tokens = tokenizer.tokenize(no_punctuation)
-		blob = TextBlob(no_punctuation,tokenizer=tokenizer)
-	
+		blob = TextBlob(no_punctuation,tokenizer=tokenizer) 
+		
 		return blob
 
-	EpUnique = sorted(subset_3.EpisodeId.unique())
+	EpUnique = sorted(master_sub_season.EpisodeId.unique())
 
 	tokens_list = []
 	
 	for EpID in EpUnique: 
-		tokens_list.append(get_ep_tokens(EpID))
+		tokens_list.append(get_ep_tokens(EpID)) # each item in list is an episode
 	
-	#for i in tokens_list:
+	# name concordance
 
-
-	def n_concordance_tokenised(text,phrase,left_margin=5,right_margin=5):
-		#concordance replication via https://simplypython.wordpress.com/2014/03/14/saving-output-of-nltk-text-concordance/
-		phraseList=phrase.split(' ')
-	 
-		c = nltk.ConcordanceIndex(text.tokens, key = lambda s: s.lower())
-		 
-		#Find the offset for each token in the phrase
-		offsets=[c.offsets(x) for x in phraseList]
-		offsets_norm=[]
-		#For each token in the phraselist, find the offsets and rebase them to the start of the phrase
-		for i in range(len(phraseList)):
-			offsets_norm.append([x-i for x in offsets[i]])
-		#We have found the offset of a phrase if the rebased values intersect
-		#--
-		# http://stackoverflow.com/a/3852792/454773
-		#the intersection method takes an arbitrary amount of arguments
-		#result = set(d[0]).intersection(*d[1:])
-		#--
-		intersects=set(offsets_norm[0]).intersection(*offsets_norm[1:])
-		 
-		concordance_txt = ([text.tokens[map(lambda x: x-left_margin if (x-left_margin) > 0 else 0,[offset])[0]:offset+len(phraseList)+right_margin]
-							for offset in intersects])
-		  
-		outputs=[''.join([x+' ' for x in con_sub]) for con_sub in concordance_txt]
-		return outputs
-	 
-	def n_concordance(txt,phrase,left_margin=5,right_margin=5):
-		tokens = nltk.word_tokenize(txt)
-		text = nltk.Text(tokens)
-	  
-		return n_concordance_tokenised(text,phrase,left_margin=left_margin,right_margin=right_margin)
+	list_of_names = labelDf.Contestant_names.apply(lambda k: k.split(" "))
 	
+	def concordance(tokenized_ep):
+
+		agg_firstname_concordance_dic = {}
+		agg_lastname_concordance_dic = {}
+		first_name_temp = []
+		last_name_temp = []
+
+		for indiv_name in list_of_names:
+			first_name, last_name = indiv_name[0], indiv_name[1]
+			print(first_name,last_name)
+			first_name_index = [ i for i, j in enumerate(tokenized_ep) if j == first_name.lower()] 
+			last_name_index = [ i for i, j in enumerate(tokenized_ep) if j == last_name.lower()] 
+
+			for name_loc in first_name_index:
+				first_name_temp.append(tokenized_ep[name_loc-3:name_loc+3])
+			agg_firstname_concordance_dic[first_name] = first_name_temp
+			#some names like 'So' kim are refercing to the word 'so' instead of name 'So', and will
+			print(agg_firstname_concordance_dic)
+			first_name_temp = []
+
+			for name_loc in last_name_index:
+				last_name_temp.append(tokenized_ep[name_loc-3:name_loc+3])
+			agg_lastname_concordance_dic[last_name] = last_name_temp	
+			last_name_temp = []
+		return (agg_firstname_concordance_dic, agg_firstname_concordance_dic)
+
+	for textblob in tokens_list:
+		firstname_concordance, lastname_concordance = concordance(textblob.tokens)
+		#dictionary obj with key = contestant names, value = concordances around name
+
+	#ngrams of concordances
+	
+	#row names contestname name and episode number
 	pdb.set_trace()
+	'''
+	subset by unique episode
+	get the Contestant_names for that episode
+	#sync up Contestant_name to that particular episode
+	problem: during special they might vote out more than one person
+
+	note: possibly also need to add metadata in the form of episode to the concordance so i can subset by episode as well
+	note: Do I tokensize before or AFTER the concordance
+	'''
+	'''
+	ModelDf_col = []
+	ModelDf_col.append()
+
+	labelDf.Contestant_names
+	master_sub_season
+	ModelDf = ModelDf[ModelDf]
+	'''
 
 
+
+
+			
+
+	# how do i want to store this?
+	#nested lists? of a list of names 
+	# or dictionarys of names with the tokens inside. then create ngrams based on the tokens
+	#pdb.set_trace()
 	
 
 
